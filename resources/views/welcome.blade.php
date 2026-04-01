ecbffb0622738733dbdfeab5c1a278ccef8b0b69
x-JOC <oshalfan@faradah.sa>
2026-04-01 06:54:54 +0300

    style: update welcome page layout and content styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -492,7 +492,7 @@
                             <div class="text-white/60 mb-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" /></svg>
                             </div>
-                            <h3 class="font-black text-2xl text-white mb-1 leading-tight text-center px-1">كتاب<br>الوقف والشركات العائلية</h3>
+                            <h3 class="font-black text-2xl text-white mb-1 leading-tight text-center px-1">الوقف<br>والشركات العائلية</h3>
                             <p class="text-[0.6rem] text-white/70 mt-auto pb-4">الفرص والتحديات ونماذج العمل</p>
                         </div>
 
